50283c52f6875a06abcc7e353b8fd015f43593cf
HKozubek <01149402@pw.edu.pl>
2021-10-25 14:01:21 +0200

    Not function

--- MinimalWorkingExample.m
+++ MinimalWorkingExample.m
@@ -1,34 +1,32 @@
-function MinimalWorkingExample()
-    addpath('D:\SerialCommunication'); % add a path to the functions
-    initSerialControl COM10 % initialise com port
-    f = figure;
-    result = zeros(300,1);
-    while(1)
-       
-        %% obtaining measurements
-        
-        measurements1 = readMeasurements(1); % read measurements from 1 to 7
-        result(1:299) = result(2:300);
-        result(300) = measurements1;
-        measurements3 = readMeasurements(3);
-        %% processing of the measurements and new control values calculation
+addpath('D:\SerialCommunication'); % add a path to the functions
+initSerialControl COM10 % initialise com port
+f = figure;
+result = zeros(300,1);
+while(1)
 
-        %% sending new values of control signals
-        sendControls([ 1, 2, 3, 4, 5, 6], ... send for these elements
-                     [ 50, 0, 0, 0, 26, 0]);  % new corresponding control values
-        
-         measurement = readMeasurements([1,5])
-         
-         plot(result)
-         title('Wyznaczenie punktu pracy')
-         xlabel('Temperatura [°C]')
-         ylabel('Czas [s]')
-         ylim([30 36])
-         xlim([1 300])
-         drawnow;
-         
-         %% synchronising with the control process
-     
-        waitForNewIteration(); % wait for new batch of measurements to be ready
-    end
+    %% obtaining measurements
+
+    measurements1 = readMeasurements(1); % read measurements from 1 to 7
+    result(1:299) = result(2:300);
+    result(300) = measurements1;
+    measurements3 = readMeasurements(3);
+    %% processing of the measurements and new control values calculation
+
+    %% sending new values of control signals
+    sendControls([ 1, 2, 3, 4, 5, 6], ... send for these elements
+                 [ 50, 0, 0, 0, 26, 0]);  % new corresponding control values
+
+     measurement = readMeasurements([1,5])
+
+     plot(result)
+     title('Wyznaczenie punktu pracy')
+     xlabel('Temperatura [°C]')
+     ylabel('Czas [s]')
+     ylim([30 36])
+     xlim([1 300])
+     drawnow;
+
+     %% synchronising with the control process
+
+    waitForNewIteration(); % wait for new batch of measurements to be ready
 end
