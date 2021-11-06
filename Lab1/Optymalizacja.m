a24b6889f5a76fa6ed368569ce09bc8f4a946807
HKozubek <01149402@pw.edu.pl>
2021-11-06 21:00:11 +0100

    Test

--- Lab1/Optymalizacja.m
+++ Lab1/Optymalizacja.m
@@ -1,3 +1,8 @@
-options = gaoptimset('StallGenLimit', 100, 'PopulationSize', 300);
-[X] = ga(@AproksSkokDMC,3,[],[],[],[],[],[],[],[],options);
+rng default
+options = optimoptions('ga','StallGenLimit', 100, 'PopulationSize', 200);
+ % For reproducibility
+%[X, fval] = ga(@AproksSkokDMC, 3); 
+[X,fval] = ga(@AproksSkokDMC,3,[],[],[],[],[],[],[],[],options);
 fprintf('\nT1=%f; T2=%f; K=%f;\n', X)
+%T1=1.608291; T2=3.442530; K=0.244211;
+%T1=1.545779; T2=69.789574; K=0.316435;
