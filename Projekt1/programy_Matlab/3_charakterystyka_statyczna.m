9e9214edb33c2fb55b2da8ed952fb464bde8bf21
michalczewskip <przemyslaw.michalczewski@wp.pl>
2021-11-06 16:01:41 +0100

    Projekt 1 - wyznaczony punkt pracy, odpowiedzi skokowe, charakterystyka statyczna i wzmocnienie, skok dla DMC, PID nie nastrojony

---
+++ Projekt1/programy_Matlab/3_charakterystyka_statyczna.m
@@ -0,0 +1,55 @@
+% wyznaczenie charakterystyki statyczcenej procesu
+clear all
+% Punkt pracy
+Upp = 0.5;
+Ypp = 4;
+
+% Ograniczenia wartości sygnału sterującego
+Umin = 0.3;
+Umax = 0.7;
+
+% Czas symulacji
+time = 300;
+
+num_sampl = 100;
+
+
+% Deklaracja macierz wejścia i wyjścia; incializacji danych początkowych
+U = zeros(time, 1);
+U(1:time) = Upp;
+Y = zeros(time, 1);
+Y(1:11) = Ypp;
+
+Ustat = zeros(num_sampl, 1);
+Ystat = zeros(num_sampl, 1);
+
+for i = 0:num_sampl
+    U(1:time) = Umin + ((Umax-Umin)/num_sampl)*i;
+    for k = 12:time
+        Y(k) =  symulacja_obiektu1Y_p1(U(k-10), U(k-11), Y(k-1), Y(k-2));
+    end
+    Ustat(i+1) = Umin + ((Umax-Umin)/num_sampl)*i;
+    Ystat(i+1) = Y(time);
+end
+
+% wzmocnienie (MNK)
+M = [ones(num_sampl+1, 1) Ustat];
+w = M\Ystat;
+
+% dla funkcji Y = a + b*U
+a = w(1);
+b = w(2);
+
+Kwzmoc = b;
+
+figure('Name', 'Charakterystyka statyczna')
+plot(Ustat, Ystat)
+ylabel('Y')
+xlabel('U')
+title("Charakterystyka statyczna Y(U); K_w_z_m_o_c = " + round(Kwzmoc, 3))
+
+matlab2tikz('charakterystyka_statyczna')
+
+
+
+
