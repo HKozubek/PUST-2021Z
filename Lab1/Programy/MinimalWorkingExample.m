ed7b2a8c869b7c8adb487c58d32326751d8f69d0
HKozubek <01149402@pw.edu.pl>
2021-11-08 14:45:44 +0100

    PID - test 3 K*1.5

--- Lab1/Programy/MinimalWorkingExample.m
+++ Lab1/Programy/MinimalWorkingExample.m
@@ -17,15 +17,15 @@ time = 1000;
 %jump1 = zeros(300, 1);
 %jump2 = zeros(300, 1);
 %jump3 = zeros(300, 1);
+% 
+% PIDu = zeros(time + 100,1);
+% PIDy = zeros(time + 100,1);
+% PIDe = zeros(time + 100,1);
 
-PIDu = zeros(time + 100,1);
-PIDy = zeros(time + 100,1);
-PIDe = zeros(time + 100,1);
 
-
-% DMCu = zeros(time + 100,1);
-% DMCy = zeros(time + 100,1);
-% DMCe = zeros(time + 100,1);
+DMCu = zeros(time + 100,1);
+DMCy = zeros(time + 100,1);
+DMCe = zeros(time + 100,1);
 
 figure(1);
 figure(2);
@@ -44,65 +44,65 @@ while(1)
     measurements3 = readMeasurements(3);
     %% processing of the measurements and new control values calculation
     
-%     PID
-    PIDy(k) = measurements1;
-    e = Yzad(k) - measurements1;
-    PIDe(k) = e;
-    E = E + e^2;
-    u = PID(e);
-    PIDu(k) = u; 
+% %     PID
+%     PIDy(k) = measurements1;
+%     e = Yzad(k) - measurements1;
+%     PIDe(k) = e;
+%     E = E + e^2;
+%     u = PID(e);
+%     PIDu(k) = u; 
     
 
 
     % DMC
-%     DMCy(k) = measurements1;
-%     e = Yzad(k) - measurements1;
-%     
-%     u = DMC(Yzad(k), measurements1, 300, 50, 10, 1);
-%     E = E + e^2;
-%     DMCe(k) = e;
-%     DMCu(k) = u; 
+    DMCy(k) = measurements1;
+    e = Yzad(k) - measurements1;
+    
+    u = DMC(Yzad(k), measurements1, 300, 60, 20, 0.1);
+    E = E + e^2;
+    DMCe(k) = e;
+    DMCu(k) = u; 
 
     %% sending new values of control signals
     sendControls([ 1, 2, 3, 4, 5, 6], ... send for these elements
-                 [ 70, 0, 0, 0, 26, 0]);  % new corresponding control values
+                 [ 50, 0, 0, 0, 26, 0]);  % new corresponding control values
 
      measurement = readMeasurements([1,5]);
      
      
-% 
-     figure(1);
-     plot(Yzad, 'r--');
-     hold on;
-     plot(PIDy,'b-');
-     xlim([1 time]);
-     ylim([28 42]);
-     legend({'Y_z_a_d','Y'})
-     title("Wyjście procesu");
-     
-     figure(2);
-     plot(PIDu,'b-');
-     xlim([1 time]);
-     ylim([0 100]);
-     legend({'U'})
-     title("Sterowanie procesu - PID");
-
-
-
+% % 
 %      figure(1);
 %      plot(Yzad, 'r--');
 %      hold on;
-%      plot(DMCy,'b-');
+%      plot(PIDy,'b-');
 %      xlim([1 time]);
 %      ylim([28 42]);
 %      legend({'Y_z_a_d','Y'})
 %      title("Wyjście procesu");
 %      
 %      figure(2);
-%      plot(DMCu,'b-');
+%      plot(PIDu,'b-');
 %      xlim([1 time]);
 %      ylim([0 100]);
-%      title("Sterowanie procesu - DMC");
+%      legend({'U'})
+%      title("Sterowanie procesu - PID");
+
+
+
+     figure(1);
+     plot(Yzad, 'r--');
+     hold on;
+     plot(DMCy,'b-');
+     xlim([1 time]);
+     ylim([28 42]);
+     legend({'Y_z_a_d','Y'})
+     title("Wyjście procesu");
+     
+     figure(2);
+     stairs(DMCu,'b-');
+     xlim([1 time]);
+     ylim([0 100]);
+     title("Sterowanie procesu - DMC");
 
 
 
--- Lab1/Programy/PID.m
+++ Lab1/Programy/PID.m
@@ -28,7 +28,7 @@ function U = PID(e)
         e2=0;
 
         % Nastawy regulatora
-        K = 0.5 * 43; %Kk = 43, Tk = 36
+        K = 0.5 * 43 * 1.5; %Kk = 43, Tk = 36
         Ti = 0.5 * 36*2;% * 4;  %inf;    10
         Td = 0.125 * 36;% * 0.6; %    0.4
         Tp = 1;
