f6a97e237b3c40a5a4c150399d30c8203506b2d1
HKozubek <01149402@pw.edu.pl>
2021-11-15 14:47:20 +0100

    Zajecia 3

--- Lab2/Programy/MinimalWorkingExample.m
+++ Lab2/Programy/MinimalWorkingExample.m
@@ -25,7 +25,7 @@ E = 0;
 %jump3 = zeros(300, 1);
 
 % zak = 10;
-% ppracy = zeros(time,1);
+ppracy = zeros(time,1);
 % skok_zak1 = zeros(time,1);
 % skok_zak2 = zeros(time,1);
 % skok_zak3 = zeros(time,1);
@@ -48,8 +48,8 @@ while(1)
     
     %jump3(1:299) = jump3(2:300);
     %jump3(300) = measurements1;
-%     ppracy(1:(time-1)) = ppracy(2:time);
-%     ppracy(time) = measurements1;
+    ppracy(1:(time-1)) = ppracy(2:time);
+    ppracy(time) = measurements1;
     
 %     skok_zak1(1:(time-1)) = skok_zak1(2:time);
 %     skok_zak1(time) = measurements1;
@@ -59,19 +59,20 @@ while(1)
     
 
     % DMC
-    DMCy(k) = measurements1;
-    e = Yzad(k) - measurements1;
-    
-    u = DMC(Yzad(k), measurements1, 300, 50, 10, 1);
-    E = E + e^2;
-    DMCe(k) = e;
-    DMCu(k) = u; 
+%     DMCy(k) = measurements1;
+%     e = Yzad(k) - measurements1;
+%     
+%     u = DMC(Yzad(k), measurements1, 300, 50, 10, 1);
+%     E = E + e^2;
+%     DMCe(k) = e;
+%     DMCu(k) = u; 
 
     %% sending new values of control signals
     sendControls([ 1, 2, 3, 4, 5, 6], ... send for these elements
-                 [ 50, 0, 0, 0, 0, 0]);  % new corresponding control values
+                 [ 50, 0, 0, 0, 26, 0]);  % new corresponding control values
+    sendControlsToG1AndDisturbance(26, 0); 
     
-    sendControlsToG1AndDisturbance(u, Zzad(k));
+%     sendControlsToG1AndDisturbance(u, Zzad(k));
 
     measurement = readMeasurements([1,5])
     
@@ -79,13 +80,13 @@ while(1)
 
 
 %      figure(1);
-     plot(Yzad, 'r--');
-     hold on;
-     plot(DMCy,'b-');
-     xlim([1 time]);
-     ylim([28 42]);
-     legend({'Y_z_a_d','Y'})
-     title("Wyjście procesu");
+%      plot(Yzad, 'r--');
+%      hold on;
+%      plot(DMCy,'b-');
+%      xlim([1 time]);
+%      ylim([28 42]);
+%      legend({'Y_z_a_d','Y'})
+%      title("Wyjście procesu");
      
 %      figure(2);
 %      stairs(DMCu,'b-');
@@ -96,12 +97,12 @@ while(1)
 
 
  
-%      plot(ppracy)
-%      title("Punkt pracy G1 = 26")
-%      xlabel('Czas [s]')
-%      ylabel('Temperatura [°C]')
-%      ylim([20 36])
-%      xlim([1 time])
+     plot(ppracy)
+     title("Punkt pracy G1 = 26")
+     xlabel('Czas [s]')
+     ylabel('Temperatura [°C]')
+     ylim([20 36])
+     xlim([1 time])
      
 %      plot(skok_zak1)
 %      title("Skok toru zaklocenie-wyjscie, G1 = 26, Z = 10")
@@ -117,8 +118,8 @@ while(1)
 
 
 
-    u
-    k = k+1;
+%     u
+%     k = k+1;
      %% synchronising with the control process
     waitForNewIteration(); % wait for new batch of measurements to be ready
 end
