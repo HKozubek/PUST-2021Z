d462440e34cf6b90f083cc110ed35438e80a689e
michalczewskip <przemyslaw.michalczewski@wp.pl>
2021-11-23 02:57:03 +0100

    Programy do wykresów z lab 2.2 (workspace test_nastawy_2/3 nie mają danych!!!)
    Signed-off-by: michalczewskip <przemyslaw.michalczewski@wp.pl>

--- Lab2/Programy/dobor_param_DMC.m
+++ Lab2/Programy/dobor_param_DMC.m
@@ -1,3 +1,29 @@
 skok1 = load('..\Workspace\dmc_test_nastawy_1.mat');
-skok2 = load('..\Workspace\dmc_test_nastawy_2.mat');
-skok3 = load('..\Workspace\dmc_test_nastawy_3.mat');
+DMCy = skok1.DMCy;
+DMCu = skok1.DMCu;
+E = skok1.E;
+Yzad = skok1.Yzad;
+Zzad = skok1.Zzad;
+
+%????? brakuje danych
+% skok2 = load('..\Workspace\dmc_test_nastawy_2.mat');
+% skok3 = load('..\Workspace\dmc_test_nastawy_3.mat');
+
+figure('Name', 'Test sterowania DMC z pomiarem zakłóceń');
+
+subplot(2,1,1)
+title("Dobór parametrów DMC: N=20; Nu=1, lambda=1")
+hold on
+stairs(Yzad, '--')
+plot(DMCy)
+xlabel('Czas [s]')
+ylabel('Y [°C]')
+xlim([0, 800])
+legend('Y_{zad}', 'Y', 'Location', 'southeast')
+
+subplot(2,1,2)
+stairs(DMCu)
+xlim([0, 800])
+xlabel('Czas [s]')
+ylabel('U [%]')
+
