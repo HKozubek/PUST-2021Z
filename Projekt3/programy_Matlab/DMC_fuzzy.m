f33a7c8dd213b52fa65ac7a47c84dcecacf1bd7f
HKozubek <01149402@pw.edu.pl>
2021-12-14 22:35:32 +0100

    Projekt 4,5

--- Projekt3/programy_Matlab/DMC_fuzzy.m
+++ Projekt3/programy_Matlab/DMC_fuzzy.m
@@ -40,24 +40,24 @@ function U = DMC_fuzzy(S_fuzzy, u, num, yzad, y, D, N, Nu, lambda, Umin, Umax)
         Upop = 0;
         dUP = zeros(D-1,1);
         
-        % kszta≈Çt funkcji dzwonowej
+        % kszta≥t funkcji dzwonowej
         interval = (Umax - Umin)/(num - 1);
         
 %       Dla num = 2:
-%         a = 0.7;                                % przedzia≈Ç warto≈õci maksymalnej
-%         b = 3;                                  % kszta≈Çt zboczy funkcji
+%         a = 0.7;                                % przedzia≥ wartoúci maksymalnej
+%         b = 3;                                  % kszta≥t zboczy funkcji
         
         %dla num = 3:
-%         a = 0.5;                                % przedzia≈Ç warto≈õci maksymalnej
-%         b = 1.5;                                  % kszta≈Çt zboczy funkcji
+%         a = 0.5;                                % przedzia≥ wartoúci maksymalnej
+%         b = 1.5;                                  % kszta≥t zboczy funkcji
         
 %         %dla num = 4:
-%         a = 0.3;                                  % przedzia≈Ç warto≈õci maksymalnej
-%         b = 1.2;                                  % kszta≈Çt zboczy funkcji
+%         a = 0.3;                                  % przedzia≥ wartoúci maksymalnej
+%         b = 1.2;                                  % kszta≥t zboczy funkcji
         
         %dla num = 5:
-        a = 0.1;                                  % przedzia≈Ç warto≈õci maksymalnej
-        b = 1.5;                                  % kszta≈Çt zboczy funkcji
+        a = 0.1;                                  % przedzia≥ wartoúci maksymalnej
+        b = 1.5;                                  % kszta≥t zboczy funkcji
 
         center = zeros(num, 1);
         
@@ -102,7 +102,7 @@ function U = DMC_fuzzy(S_fuzzy, u, num, yzad, y, D, N, Nu, lambda, Umin, Umax)
     Y = y*ones(N,1);
     
     for j = 1:num
-        % tu ju≈º online
+        % tu juø online
         Y0 = Y + Mp{j}*dUP;
         dU = K{j}*(Yzad - Y0);
         du = dU(1); 
@@ -115,7 +115,7 @@ function U = DMC_fuzzy(S_fuzzy, u, num, yzad, y, D, N, Nu, lambda, Umin, Umax)
     end
     U = U/sum(w);
   
-    du = U - Upop;                      %wa≈ºne, ≈ºeby jeszcze raz wyliczyƒá
+    du = U - Upop;                      %waøne, øeby jeszcze raz wyliczyÊ
     for n=D-1:-1:2
         dUP(n) = dUP(n-1);
     end
